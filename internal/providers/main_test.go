3ec7f57f15cc9711135f8cb593a0d886152e7f27
Mario-F <github@fritschen.net>
2022-03-02 21:20:07 +0100

    test: added noise for ipv6
    Signed-off-by: Mario-F <github@fritschen.net>

--- internal/providers/main_test.go
+++ internal/providers/main_test.go
@@ -14,6 +14,9 @@ func TestCaptureIPv6(t *testing.T) {
 				Inputs: []string{
 					"Your IPv6 address on the public Internet appears to be 2001:9e8:e169:4400:23f5:f330:350b:7e80",
 					"2001:9e8:e169:4400:23f5:f330:350b:7e80 is your ip address",
+					"2001:9e8:e169:4400:23f5:f330:350b:7e80right noise",
+					"left noise2001:9e8:e169:4400:23f5:f330:350b:7e80",
+					"all2001:9e8:e169:4400:23f5:f330:350b:7e80noise",
 				},
 				Output: "2001:9e8:e169:4400:23f5:f330:350b:7e80",
 			},
